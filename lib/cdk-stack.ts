9d0563090062b854ffa9b7bc4f325761e7854e0e
Alfonso Roman Zubeldia <alfonsoromanz24@gmail.com>
2023-07-31 20:57:22 -0300

    Functional stack

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -3,16 +3,18 @@ import * as rds from '@aws-cdk/aws-rds';
 import * as secretsmanager from '@aws-cdk/aws-secretsmanager';
 import * as iam from '@aws-cdk/aws-iam';
 import * as ec2 from '@aws-cdk/aws-ec2';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
+require('dotenv').config()
 
 export class RdsProxyStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // Assuming an existing VPC
-    const vpc = ec2.Vpc.fromLookup(this, 'VPC', { isDefault: true });
+    const vpc = ec2.Vpc.fromVpcAttributes(this, 'VPC', {
+      vpcId: process.env.VPC_ID!, 
+      availabilityZones: [process.env.AVAILABILITY_ZONES!],
+      isolatedSubnetIds: String(process.env.PRIVATE_SUBNET_IDS!).split(',')
+    });
 
     // Create a new security group
     const securityGroup = new ec2.SecurityGroup(this, 'DBSecurityGroup', {
@@ -22,30 +24,18 @@ export class RdsProxyStack extends cdk.Stack {
     });
 
     // Assuming an existing RDS DB instance
-    const dbInstanceIdentifier = 'existing-db-instance-identifier';
-    const dbInstance = rds.DatabaseInstance.fromDatabaseInstanceAttributes(this, 'ExistingDBInstance', {
+    const dbInstanceIdentifier = process.env.DB_IDENTIFIER!;
+    const dbInstance = rds.DatabaseInstance.fromDatabaseInstanceAttributes(this, 'paymentsDBInstance', {
       instanceIdentifier: dbInstanceIdentifier,
-      instanceEndpointAddress: 'existing-db-instance-endpoint',
+      instanceEndpointAddress:  process.env.DB_ENDPOINT!,
       port: 3306,
       securityGroups: [securityGroup],
+      engine: rds.DatabaseInstanceEngine.POSTGRES
     });
 
-    // Database credentials from environment variables
-    const dbUsername = process.env.DB_USERNAME || '';
-    const dbPassword = process.env.DB_PASSWORD || '';
-
-    // Create a new secret in Secrets Manager for the database credentials
-    const dbSecret = new secretsmanager.Secret(this, 'DBSecret', {
-      secretName: 'dbSecret',
-      generateSecretString: {
-        secretStringTemplate: JSON.stringify({
-          username: dbUsername,
-        }),
-        generateStringKey: 'password',
-        excludePunctuation: true,
-        passwordLength: 30,
-      },
-    });
+    // This code assumes a secret called 'payments-db-secret' already exists
+    // It is a good practice since we don't want to expose credentials in a cloudformation template
+    const dbSecret = secretsmanager.Secret.fromSecretNameV2(this, 'DBSecret', 'payments-db-secret');
 
     // IAM role for RDS Proxy
     const proxyRole = new iam.Role(this, 'ProxyRole', {
@@ -67,26 +57,27 @@ export class RdsProxyStack extends cdk.Stack {
     const proxy = new rds.DatabaseProxy(this, 'Proxy', {
       vpc: vpc,
       secrets: [dbSecret],
-      dbProxyName: 'my-proxy',
+      dbProxyName: 'payments-rds-proxy',
       role: proxyRole,
       vpcSubnets: {
         subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
       },
       proxyTarget: rds.ProxyTarget.fromInstance(dbInstance),
       requireTLS: false,
-      idleClientTimeout: cdk.Duration.minutes(30),
+      //idleClientTimeout: cdk.Duration.minutes(30),
       debugLogging: false,
     });
   }
 }
 
-// Define the CDK application
-class MyCdkApp extends cdk.App {
-  constructor() {
-    super();
-    new RdsProxyStack(this, 'RdsProxyStack');
-  }
-}
+// // /Define the CDK application
+// class MyCdkApp extends cdk.App {
+//   constructor() {
+//     super();
+//     new RdsProxyStack(this, 'RdsProxyStack');
+//   }
+// }
 
-// Instantiate and run the CDK application
-new MyCdkApp().synth();
+// // Instantiate and run the CDK application
+// new MyCdkApp().synth();
+
